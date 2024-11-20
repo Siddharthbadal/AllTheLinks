43021659a411152dddb6ad2a5a39a9bc8052b19f
siddharthbadal <siddharthabadal.2015@gmail.com>
2024-11-20 16:26:28 +0530

    commit

---
+++ src/app/components/MyPDFs.jsx
@@ -0,0 +1,101 @@
+import Link from "next/link"
+
+export default function MyPDFs() {
+  return (
+    <div className=" flex flex-col mb-16">
+        <Link href="/pdf/NothingMattersbyCarles.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        Nothing Matters by Carles
+        </Link> 
+        <Link href="/pdf/TheGodDelusion.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The God Delusion
+        </Link> 
+        <Link href="/pdf/TheElephaintInTheBrain.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The Elephaint In The Brain
+        </Link> 
+        <Link href="/pdf/ItDidn'tStartWithYou.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        It Didn't Start With You
+        </Link> 
+        <Link href="/pdf/TheAlmanackofNavalRavikant.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The Almanack of Naval Ravikant
+        </Link> 
+        <Link href="/pdf/Nietzsche-Beyond-Good-and-Evil.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+            Beyond Good And Evil
+        </Link> 
+        <Link href="/pdf/FooledByRandomness.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        FooledByRandomness
+        </Link> 
+        <Link href="/pdf/OutwittingTheDevil.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        Outwitting the Devil
+        </Link> 
+        <Link href="/pdf/TheMeditationsofMarcusAurelius.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        TheMeditationsofMarcusAurelius
+        </Link> 
+        <Link href="/pdf/The-way-of-the-superior-man.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The Way Of Superior Man
+        </Link> 
+        <Link href="/pdf/SkinInTheGame.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        Skin in the Game 
+        </Link> 
+        <Link href="https://hpmor.com/" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        Harry Potter and The Methods of Rationality
+        </Link> 
+        <Link href="https://arxiv.org/pdf/2407.06214" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        Guarded Successor: A Novel Temporal Logic
+        </Link> 
+        <Link href="https://www.andrewburke.me/media/Gracian.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+            THE ART OF WORLDLY WISDOM
+        </Link> 
+        <Link href="https://oms-inet.files.svdcdn.com/production/files/JEL-v2.0.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        Agent-Based Modeling in Economics and Finance
+        </Link> 
+        <Link href="https://www.cia.gov/readingroom/docs/CIA-RDP96-00788R001700210016-5.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+            Analysis and Assessment of Gateway process
+        </Link> 
+        <Link href="https://www.princeton.edu/~dixitak/home/Elaine-Final-Web.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        An Option Value Problem from Seinfeld
+        </Link> 
+        <Link href="https://astronomy.org/moravian/C00-Last%20Question.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The Last Question Isaac Asimov
+        </Link> 
+        <Link href="https://lamport.azurewebsites.net/pubs/lamport-paxos.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The Part-Time Parliament
+        </Link> 
+        <Link href="https://www.researchgate.net/publication/228797158_How_complex_systems_fail" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        How Complex System fails
+        </Link> 
+        <Link href="https://www.rand.org/content/dam/rand/pubs/reports/2008/R3765.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        First Strike Stability
+        </Link> 
+        <Link href="https://bitcoin.org/bitcoin.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        Bitcoin: A Peer-to-Peer Electronic Cash System
+        </Link> 
+        <Link href="https://cran.r-project.org/web/packages/psyphy/psyphy.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        Functions for Analyzing Psychophysical Data in R- psyphy
+        </Link> 
+        <Link href="https://gandalf.fee.urv.cat/professors/AntonioQuesada/Curs1920/Cipolla_laws.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        THE BASIC LAWS OF HUMAN STUPIDITY
+        </Link> 
+        <Link href="https://targetedindividualscanada.com/wp-content/uploads/2011/12/monarch-the-new-phoenix-program.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        MONARCH: THE NEW PHOENIX PROGRAM
+        </Link> 
+        <Link href="https://www.docdroid.net/4H1DgHE/nac-protocol-printable-pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The NAC Protocol
+        </Link> 
+        <Link href="https://dergigi.com/assets/files/UO-Economics.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The In-game Economics of Ultima Online
+        </Link> 
+        <Link href="https://med.emory.edu/departments/neurology/_documents/cousteau_swimming_sharks.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        HOW TO SWIM WITH SHARKS: A PRIMER
+        </Link> 
+        <Link href="https://www.maths.ed.ac.uk/~v1ranick/papers/wigner.pdf" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        THE UNREASONABLE EFFECTIVENSS OF MATHEMATICS IN THE NATURAL SCIENCES
+        </Link> 
+        <Link href="https://www2.imm.dtu.dk/pubdb/pubs/3274-full.html" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        The Matrix Cookbook
+        </Link> 
+        <Link href="https://arxiv.org/pdf/1409.0003" target="_blank" className="font-medium mb-2 tracking-wider hover:text-gray-700">
+        What You Should Know About Megaprojects, and Why:
+        </Link> 
+    </div>
+  )
+}
