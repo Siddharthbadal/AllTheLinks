94c71c4b8dc7d5c39460b655694689f0167119bc
siddharthbadal <siddharthabadal.2015@gmail.com>
2026-01-07 21:11:22 +0530

    new

--- src/data/links.js
+++ src/data/links.js
@@ -1,188 +1,166 @@
-export default  [
+export default [
     {
-        "link":"https://0ducks.wordpress.com/wp-content/uploads/2015/01/the-lazy-mans-guide-to-enlightenment-by-thaddeus-golas.pdf",
+        "link": "https://0ducks.wordpress.com/wp-content/uploads/2015/01/the-lazy-mans-guide-to-enlightenment-by-thaddeus-golas.pdf",
         "type": "Spirituality",
-        "title":"The Lazy Man's Guide to Enlightenment"
-    },
-    {
-        "link":"https://libraryofagartha.com/Philosophy/Traditionalism/Romanian/Emil%20Cioran/Cioran,%20E.%20M.%20-%20Trouble%20With%20Being%20Born%20(Seaver,%202011).pdf",
-        "type": "philosophy ",
-        "title":"The Trouble with Being Born : E. M. Cioran"
+        "title": "The Lazy Man's Guide to Enlightenment"
     },
     {
         "link": "http://cs.gettysburg.edu/~duncjo01/assets/writings/library/NothingMattersbyCarles.pdf",
         "type": "",
-        "title":"Nothing Matters by Carles"
+        "title": "Nothing Matters by Carles"
     },
     {
         "link": "https://lab.cccb.org/en/the-i-in-the-internet/",
         "type": "Article",
-        "title":"The I in the Internet"
+        "title": "The I in the Internet"
     },
     {
         "link": "https://hpmor.com/",
         "type": "",
-        "title":"Harry Potter and The Methods of Rationality"
+        "title": "Harry Potter and The Methods of Rationality"
     },
-    {
-        "link": "http://bulletin-archive.kenyon.edu/x4280.html",
-        "type": "Speech",
-        "title":"This is Water David Foster Wallace"
-    },
-	
+
     {
         "link": "https://www.planetpublish.com/wp-content/uploads/2011/11/Notes_from_the_Underground_NT.pdf",
         "type": "story",
-        "title":"Notes from the Underground Fyodor Dostoevsky"
+        "title": "Notes from the Underground Fyodor Dostoevsky"
     },
     {
         "link": "https://www.andrewburke.me/media/Gracian.pdf",
         "type": "",
-        "title":"The art of worldly wisdom"
+        "title": "The art of worldly wisdom"
     },
     {
         "link": "https://openlibrary-repo.ecampusontario.ca/jspui/bitstream/123456789/1145/4/The-Bell-Jar-1645639688._print.pdf",
         "type": "article",
-        "title":"The Bell Jar : Sylvia Plath"
+        "title": "The Bell Jar : Sylvia Plath"
     },
     {
         "link": "https://www.newyorker.com/magazine/2017/02/13/when-things-go-missing",
         "type": "",
-        "title":"When Things Go Missing"
+        "title": "When Things Go Missing"
     },
-    
+
     {
         "link": "https://whatrocks.github.io/commencement-db/1983-margaret-atwood-university-of-toronto/",
         "type": "Speech",
-        "title":"Attitude Margaret Atwood"
-    },    
+        "title": "Attitude Margaret Atwood"
+    },
     {
         "link": "https://astronomy.org/moravian/C00-Last%20Question.pdf",
         "type": "story",
-        "title":"The Last Question Isaac Asimov"
+        "title": "The Last Question Isaac Asimov"
     },
-    
+
     {
         "link": "https://thoughtcatalog.com/sheila-heti/2013/07/why-go-out/",
         "type": "article",
-        "title":"Why go out?"
-    },
-            
-    {
-        "link": "https://bitcoin.org/bitcoin.pdf",
-        "type": "paper",
-        "title":"Bitcoin: A Peer-to-Peer Electronic Cash System"
+        "title": "Why go out?"
     },
     {
         "link": "https://www.susanorlean.com/articles/place_to_disappear.html",
         "type": "story",
-        "title":"The Place to Disappear"
+        "title": "The Place to Disappear"
     },
-        
+
     {
         "link": "https://gandalf.fee.urv.cat/professors/AntonioQuesada/Curs1920/Cipolla_laws.pdf",
         "type": "essay",
-        "title":"The Basic law of stupidity"
+        "title": "The Basic law of stupidity"
     },
-        
+
     {
         "link": "http://sriramk.com/memos/elop-burning-platforms.pdf",
         "type": "story",
-        "title":"Burning Platforms : Stephen Elop"
+        "title": "Burning Platforms : Stephen Elop"
     },
-  
+
     {
         "link": "https://www.eastoftheweb.com/",
         "type": "Website",
-        "title":"Short stories from around the world"
-    },    
-                            
+        "title": "Short stories from around the world"
+    },
+    {
+        "link": "https://ia803405.us.archive.org/32/items/thus-spoke-zarathustra/Thus%20spoke%20Zarathustra.pdf",
+        "type": "Website",
+        "title": "Thus Spoke Zarathustra - Nietzsche"
+    },
+
     {
         "link": "https://www.navalmanack.com/almanack-of-naval-ravikant/table-of-contents",
         "type": "Book",
-        "title":"The Almanack of Naval Ravikant"
+        "title": "The Almanack of Naval Ravikant"
     },
     {
         "link": "/pdf/Ernest-Becker-The-Denial-of-Death.pdf",
         "type": "Book",
-        "title":"The Denial of Death"
-    },
-    {
-        "link": "https://www.runnersworld.com/advanced/a20794176/an-elite-state-of-mind/?page=single",
-        "type": "article",
-        "title":"An Elite State of Mind"
+        "title": "The Denial of Death"
     },
     {
         "link": "/pdf/The-way-of-the-superior-man.pdf",
         "type": "Spirituality",
-        "title":"The way of the superior man"
+        "title": "The way of the superior man"
     },
-    
+
     {
         "link": "/pdf/TheGameofLife-FlorenceScovelShinn.pdf",
         "type": "Book",
-        "title":"The Game of Life : Florence Scovel Shinn"
+        "title": "The Game of Life : Florence Scovel Shinn"
     },
     {
         "link": "/pdf/FacebookLittleRedBook.pdf",
         "type": "paper",
-        "title":"Facebook Little RedBook"
+        "title": "Facebook Little RedBook"
     },
     {
         "link": "/pdf/ItDidn'tStartWithYou.pdf",
         "type": "book",
-        "title":"It Didn't Start With You"
+        "title": "It Didn't Start With You"
     },
     {
         "link": "/pdf/AfterLifebyJoanDidion.pdf",
         "type": "article",
-        "title":"After Life : Joan Didion"
+        "title": "After Life : Joan Didion"
     },
     {
         "link": "/pdf/OnTheShortnessofLife-Seneca.pdf",
         "type": "essay",
-        "title":"On The Shortness of Life : Seneca"
+        "title": "On The Shortness of Life : Seneca"
     },
     {
         "link": "/pdf/NothingMattersbyCarles.pdf",
         "type": "Spirituality",
-        "title":"Nothing Matters by Carles"
+        "title": "Nothing Matters by Carles"
     },
     {
         "link": "https://archive.org/details/5FooledByRandomnessNassimTaleb/page/n131/mode/2up?view=theater",
         "type": "",
-        "title":"Fooled By Randomness : Nassim taleb"
+        "title": "Fooled By Randomness - Taleb"
     },
-              		
-	{
+
+    {
         "link": "https://www.goldennumber.net/wp-content/uploads/pepsi-arnell-021109.pdf",
         "type": "paper",
-        "title":" Pepsi Logo Design"
+        "title": " Pepsi Logo Design"
     },
-		
-	{
+
+    {
         "link": "https://dutraeconomicus.wordpress.com/wp-content/uploads/2014/02/you-are-not-so-smart.pdf",
         "type": "book",
-        "title":" You are not so smart"
+        "title": " You are not so smart"
     },
-	{
-        "link": "https://www.gutenberg.org/cache/epub/13402/pg13402-images.html",
-        "type": "Book",
-        "title":" The Hindu-Yogi Science of Breath"
-    },
-	
-	{
+    {
         "link": "/pdf/The-Psychology-of-Money.pdf",
         "type": "Finance",
-        "title":" The Psychology of Money"
+        "title": " The Psychology of Money"
     },
-	{
+    {
         "link": "https://www.julian.com/blog/life-planning",
         "type": "Blog",
-        "title":" What to do with your life"
+        "title": " What to do with your life"
     },
-	
-    
-	
-    
+
+
+
+
 ]
